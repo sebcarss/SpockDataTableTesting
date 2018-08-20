f112e1fde5a34fdea7e7e35be8356322ebbdd71c
Seb Carss <seb.carss@gmail.com>
2018-08-20 23:08:04 +0100

    Refactor DateUtilsTest to use data tables
    This is required to de-dupe the test cases and to make it easier
    to read for future developers.

--- src/test/groovy/testing/utils/DateUtilsTest.groovy
+++ src/test/groovy/testing/utils/DateUtilsTest.groovy
@@ -8,33 +8,19 @@ class DateUtilsTest extends Specification {
 
     def DateUtils utils = new DateUtils()
 
-    def "Current date is before start date" () {
-        given: "Start date and end date are in the future"
-
-        def startDate = now().plusDays(2)
-        def endDate = now().plusDays(2)
-
-        when: "Start date is checked to see if in the past"
+    def "Date is not current" () {
+        when: "Current date is not between the start date and the end date"
 
         def result = utils.isCurrent(startDate, endDate)
 
         then: "Returned result is false"
 
-        result == false
-    }
+        assert result == false : message
 
-    def "Current date is after end date" () {
-        given: "Start date and end date are in the past"
+        where:
 
-        def startDate = now().minusDays(2)
-        def endDate = now().minusDays(2)
-
-        when: "End date is checked to see if in the past"
-
-        def result = utils.isCurrent(startDate, endDate)
-
-        then: "Returned result is false"
-
-        result == false
+        startDate                | endDate                  | message
+        now().plusDays(2)  | now().plusDays(2)  | "Start date is in the future"
+        now().minusDays(2) | now().minusDays(2) | "End date is in the past"
     }
 }
