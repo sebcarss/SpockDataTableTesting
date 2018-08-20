fa6fc1e706708d3da20308e9d44a88e221da7402
Seb Carss <seb.carss@gmail.com>
2018-08-20 23:31:03 +0100

    Add valid start and end date check
    This change adds a simple check to make sure that the isCurrent
    method of DateUtils will successfully return true when the
    startDate is before the current time and the current time is
    before the endDate. As the code was already completed as part
    of other tests, this code is only a test change.

--- src/test/groovy/testing/utils/DateUtilsTest.groovy
+++ src/test/groovy/testing/utils/DateUtilsTest.groovy
@@ -8,19 +8,20 @@ class DateUtilsTest extends Specification {
 
     def DateUtils utils = new DateUtils()
 
-    def "Date is not current" () {
-        when: "Current date is not between the start date and the end date"
+    def "Checks for current date" () {
+        when: "Start date and end date are passed into isCurrent method"
 
         def result = utils.isCurrent(startDate, endDate)
 
-        then: "Returned result is false"
+        then: "Returned result is expected from data table"
 
-        assert result == false : message
+        assert result == expected : message
 
         where:
 
-        startDate                | endDate                  | message
-        now().plusDays(2)  | now().plusDays(2)  | "Start date is in the future"
-        now().minusDays(2) | now().minusDays(2) | "End date is in the past"
+        startDate                | endDate                  | expected | message
+        now().plusDays(2)  | now().plusDays(2)  | false    | "Start date is in the future"
+        now().minusDays(2) | now().minusDays(2) | false    | "End date is in the past"
+        now().minusDays(2) | now().plusDays(2)  | true     | "Dates are current"
     }
 }
