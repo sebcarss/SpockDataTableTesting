2676544656bb2726cd6dc1addac12d2f6ba4799b
Seb Carss <seb.carss@gmail.com>
2018-08-20 22:19:03 +0100

    Add valid end date check
    This change adds an end date parameter to the isCurrent method in the
    DateUtils class and with this parameter we can test that a start and
    end date envelope the current date, if they do not then the test fails
    and false is returned.

--- src/main/java/testing/utils/DateUtils.java
+++ src/main/java/testing/utils/DateUtils.java
@@ -6,11 +6,11 @@ import static java.time.LocalDateTime.now;
 
 public class DateUtils {
 
-    public boolean isCurrent(LocalDateTime startDate) {
-        if (now().isBefore(startDate)) {
-            return false;
-        } else {
+    public boolean isCurrent(LocalDateTime startDate, LocalDateTime endDate) {
+        if (startDate.isBefore(now()) && now().isBefore(endDate)) {
             return true;
+        } else {
+            return false;
         }
     }
 }
--- src/test/groovy/testing/utils/DateUtilsTest.groovy
+++ src/test/groovy/testing/utils/DateUtilsTest.groovy
@@ -2,20 +2,36 @@ package testing.utils
 
 import spock.lang.Specification
 
-import java.time.LocalDateTime
+import static java.time.LocalDateTime.now
 
 class DateUtilsTest extends Specification {
 
     def DateUtils utils = new DateUtils()
 
     def "Current date is before start date" () {
-        given: "Start date is in the future"
+        given: "Start date and end date are in the future"
 
-        def startDate = LocalDateTime.now().plusDays(2)
+        def startDate = now().plusDays(2)
+        def endDate = now().plusDays(2)
 
         when: "Start date is checked to see if in the past"
 
-        def result = utils.isCurrent(startDate)
+        def result = utils.isCurrent(startDate, endDate)
+
+        then: "Returned result is false"
+
+        result == false
+    }
+
+    def "Current date is after end date" () {
+        given: "Start date and end date are in the past"
+
+        def startDate = now().minusDays(2)
+        def endDate = now().minusDays(2)
+
+        when: "End date is checked to see if in the past"
+
+        def result = utils.isCurrent(startDate, endDate)
 
         then: "Returned result is false"
 
